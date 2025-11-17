cb58fc106187888898bc81efb5e070586e1accf3
André Lamego <andreolamego@gmail.com>
2025-11-17 20:37:27 -0300

    update model classes

---
+++ src/main/java/fatec/lanchoneteapp/core/domain/model/Cliente.java
@@ -0,0 +1,31 @@
+public class Cliente {
+  String nome;
+String tel;
+String logradouro;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+}
+
---
+++ src/main/java/fatec/lanchoneteapp/core/domain/model/Produto.java
@@ -0,0 +1,42 @@
+class Produto{
+
+int idProd;
+String nome;
+long qntdEstoq;
+long valorUn;
+
+    public int getIdProd() {
+        return idProd;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public long getQntdEstoq() {
+        return qntdEstoq;
+    }
+
+    public long getValorUn() {
+        return valorUn;
+    }
+
+    public void setIdProd(int idProd) {
+        this.idProd = idProd;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setQntdEstoq(long qntdEstoq) {
+        this.qntdEstoq = qntdEstoq;
+    }
+
+    public void setValorUn(long valorUn) {
+        this.valorUn = valorUn;
+    }
+
+
+
+}
