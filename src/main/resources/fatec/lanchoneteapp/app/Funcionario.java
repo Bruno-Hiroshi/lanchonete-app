e2d13824493169440d7dde6b55ddddf147867bc6
Bruno-Hiroshi <hiroshixdyu@gmail.com>
2025-11-17 15:57:51 -0300

    Update Funcionario.java

--- src/main/resources/fatec/lanchoneteapp/app/Funcionario.java
+++ src/main/resources/fatec/lanchoneteapp/app/Funcionario.java
@@ -1,3 +1,41 @@
+import java.time.LocalDate;
 public class Funcionario {
+  String nome;
+    Cargo cargo;
+    String tel;
+    LocalDate dataContrat;
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public LocalDate getDataContrat() {
+        return dataContrat;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setDataContrat(LocalDate dataContrat) {
+        this.dataContrat = dataContrat;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
 
 }
+
